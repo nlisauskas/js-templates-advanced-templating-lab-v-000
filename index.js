bccb3798d35e9c087f2b2003e0163db3763dc687
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-15 19:27:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,20 @@ function getRecipeVals() {
   return(recipe)
 }
 
+function handlebarsSetup() {
+  //put any handlebars registrations here.
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+}
 
+function init() {
+   //put any page initialization/handlebars initialization here
+   handlebarsSetup()
+   initForm()
+}
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
