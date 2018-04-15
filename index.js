ca4a22be3e2f32154fe6ac0a1016b96b1ee67269
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-15 19:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML;
+
+  var template = Handlebars.compile(formTemplate);
+
+  document.getElementByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
+
+function createRecipe() {
+  var recipe = getRecipeVals()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
